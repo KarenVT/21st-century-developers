c6a0601475104bc75be0c0bb82cc49d0a34b9eb6
sebaspedroza <sebasp0814@misena.edu.co>
2021-10-13 12:06:09 -0500

    Diseño login

--- src/pages/Registro_login/Login.jsx
+++ src/pages/Registro_login/Login.jsx
@@ -4,41 +4,48 @@ import { Link } from "react-router-dom";
 const Login = () => {
     return (
         <div className="h-screen flex flex-col w-full justify-center items-center bg-yellow-600">
+            <form className="mt-8 max-w-md w-screen bg-white px-20 py-10 rounded-full">
             <h2 className="m-4 text-center text-3xl font-extrabold text-gray-900">INICIA SESIÓN EN TU CUENTA</h2>
-            <form className="mt-8 max-w-md">
                 <div>
                     <p>Usuario</p>
-                    <input className="appearance-none px-3 py-2 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" type="email" placeholder="@c.com" required />
+                    <input className="w-full appearance-none px-3 py-2 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" type="email" placeholder="@c.com" required />
                 </div>
-                <div>
+                <div >
                     <p>Contraseña</p>
-                    <input className="appearance-none px-3 py-2 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" type="password" required />
+                    <input className="w-full appearance-none px-3 py-2 border border-gray-300 text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" type="password" required />
                 </div>
 
-                <div >
-                    <label htmlFor=" recuérdame ">
-                        <input type="checkbox" name=" recuerdame " />
-                        Recuérdame
-                    </label>
+                <div className="flex justify-between py-2">
+                    <div className="">
+                        <label htmlFor=" recuérdame ">
+                            <input type="checkbox" name=" recuerdame " />
+                            Recuérdame
+                        </label>
+                    </div>
+                    <div className="">
+                        <Link to="/">
+                            Olvidaste tu contraseña
+                        </Link>
+                    </div>
+                </div>
+                <div className="flex justify-between">
+                    <div className="">
+                        <Link to="/">
+                            <button className="bg-indigo-500 p-2 text-white rounded-full shadow-md hover:bg-indigo-700" type="submit">Iniciar sesión</button>
+                        </Link>
+                    </div>
+                    <div className="">
+                        <Link to="/Registro">
+                            <button className="bg-indigo-500 p-2  text-white rounded-full shadow-md hover:bg-indigo-700">Registrarse</button>
+                        </Link>
+                    </div>
                 </div>
                 <br />
-                <div className="bg-indigo-500 p-2 text-white rounded-lg    shadow-md hover:bg-indigo-700">
-                    <Link to="/">
-                        Olvidaste tu contraseña
-                    </Link>
+                <div className="flex justify-center">
+                    <div className="px-2">
+                        <button className=" bg-indigo-500 p-2 text-white rounded-full    shadow-md hover:bg-indigo-700">Continua con Google</button>
+                    </div>
                 </div>
-                <br />
-                <div className="">
-               
-                    <Link to="/">
-                        <button className="bg-indigo-500 p-2 text-white rounded-lg    shadow-md hover:bg-indigo-700" type="submit">Iniciar sesión</button>
-                    </Link>
-                </div>
-                <br />
-                <div>
-                    <button className="bg-indigo-500 p-2 text-white rounded-lg    shadow-md hover:bg-indigo-700">Continua con Google</button>
-                </div>
-                
             </form>
         </div>
     )
