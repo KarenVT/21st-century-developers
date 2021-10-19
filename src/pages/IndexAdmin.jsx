c86fdbc59bf53c9df4929a5174a8ffd6d86444ac
sebaspedroza <sebasp0814@misena.edu.co>
2021-10-18 21:00:37 -0500

    cambios en le Index y el Navbar

--- src/pages/IndexAdmin.jsx
+++ src/pages/IndexAdmin.jsx
@@ -3,18 +3,23 @@ import { Link } from "react-router-dom";
 
 const Admin = () => {
     return (
-        <div className="flex justify-around">
-            <div className="img  flex flex-col ">
-                <Link to="/admin/RegistroProductos">
-                    <button className="text-white  text-4xl  bg-indigo-500 p-2 rounded-lg  hover:bg-indigo-700">Master productos</button>
-                </Link>
-            </div>
-            <div className="img2 flex-col  ">
-                <Link to="/Admin/Ventas">
-                    <button className="text-white  text-4xl  bg-indigo-500 p-2 rounded-lg  hover:bg-indigo-700">Master ventas</button>
-                </Link>
 
-            </div>
+        <div className="flex justify-around">
+            <Link to="/admin/RegistroProductos" className="mt-32 m-9">
+                <button className=" text-white  text-4xl bg-indigo-500 p-2 rounded-full  hover:bg-indigo-700 mt-20 ">Master productos</button>
+            </Link>
+
+            {/* <div className="img  flex flex-col items-center mt-44">
+
+            </div> */}
+            
+            <Link to="/Admin/Ventas" className="mt-32 ">
+                <button className="text-white  text-4xl  bg-indigo-500 p-2 rounded-full  hover:bg-indigo-700 mt-20 ">Master ventas</button>
+            </Link>
+            {/* <div className="img2 flex-col flex items-center mt-44">
+
+
+            </div> */}
         </div>
     )
 };
