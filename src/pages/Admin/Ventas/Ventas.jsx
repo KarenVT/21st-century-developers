ab04bdaf7738dc91f5723c671f24d4a54c726bb5
KarenVT <karenkvot@gmail.com>
2021-10-18 20:53:34 -0500

    delete axios

--- src/pages/Admin/Ventas/Ventas.jsx
+++ src/pages/Admin/Ventas/Ventas.jsx
@@ -5,310 +5,13 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-// const Ventas = () => {
-
-//     const [mostrarLista, setMostrarLista] = useState(true);
-//     const [venta, setVentas] = useState([]);
-//     const [cambioBoton, setCambioBoton] = useState('Ver lista de Ventas');
-
-//     useEffect(() => {
-//         // datos desde el backend
-//         // const getVentas = async () => {
-
-//         //     const options = { method: 'GET', url: 'http://localhost:3000/admin/ventas' };
-
-//         //     axios.request(options).then(function (response) {
-//         //         console.log(response.data);
-//         //         setVentas(response.data);
-//         //     }).catch(function (error) {
-//         //         console.error(error);
-//         //     });
-//         // }
-
-//         // if (mostrarLista) {
-//         //     getVentas();
-//         // }
-//         console.log("mostar:",mostrarLista, venta);
-//     }, []);
-
-//     useEffect(() => {
-//         // movernos de registros a listas de ventas por medio de un boton
-//         return () => {
-//             if (mostrarLista) {
-//                 setCambioBoton('Ver lista de Ventas');
-//             } else {
-//                 setCambioBoton('Registrar Venta');
-//             }
-//         }
-//     }, [mostrarLista]);
-
-//     return (
-//         <div className="flex h-full w-full flex-col items-center justify-start p-10">
-//             <div className="flex flex-col items-center">
-//                 <h1 className="bg-paleta5 bg-opacity-50 text-4xl m-5 p-5 text-paleta6">
-//                     Área de Administación de Ventas
-//                 </h1>
-//                 {mostrarLista ? (
-//                     <ListVentas
-//                         listaventas={venta}
-//                     />
-//                 ) : (
-//                     <RegisVentas
-//                         setMostrarLista={setMostrarLista}
-//                         listaventas={venta}
-//                         setVentas={setVentas}
-//                     />
-//                 )}
-//                 <button
-//                     onClick={() => {
-//                         setMostrarLista(!mostrarLista)
-//                     }}
-//                     className=" rounded-full h-10 w-2/6 mt-5 bg-paleta2 bg-opacity-80 text-paleta6 shadow-lg"
-//                 >
-//                     {cambioBoton}
-//                 </button>
-//             </div>
-//         </div>
-//     )
-// }
-
-// const RegisVentas = ({ setMostrarLista, listaventas, setVentas }) => {
-
-//     // UseRef Hook le permite conservar valores. Se puede usar para almacenar un valor mutable que no causa una repetición cuando se actualiza.
-//     const form = useRef(null);
-
-//     const submitForm = async (e) => {
-//         e.preventDefault();
-
-//         const nv = new FormData(form.current);
-
-//         const nuevaVenta = {};
-//         nv.forEach((value, key) => {
-//             nuevaVenta[key] = value;
-//         });
-
-//         // const options = {
-//         //     method: 'POST',
-//         //     url: 'http://localhost:3000/admin/ventas/nuevo',
-//         //     headers: { 'Content-Type': 'application/json' },
-//         //     data: {
-//         //         fecha: nuevaVenta.fecha,
-//         //         idProducto: nuevaVenta.idProducto,
-//         //         id: nuevaVenta.id,
-//         //         nombreProducto: nuevaVenta.nombreProducto,
-//         //         idCliente: nuevaVenta.idCliente,
-//         //         cantidadProducto: nuevaVenta.cantidadProducto,
-//         //         nombreCliente: nuevaVenta.nombreCliente,
-//         //         precioUnitario: nuevaVenta.precioUnitario,
-//         //         nombreVendedor: nuevaVenta.nombreVendedor,
-//         //         totalVenta: nuevaVenta.totalVenta,
-//         //     }
-//         // };
-
-//         // await axios
-//         //     .request(options).then(function (response) {
-//         //         console.log(response.data);
-//         //     }).catch(function (error) {
-//         //         console.error(error);
-//         //     });
-
-//             setMostrarLista(true);
-//             setMostrarLista([...listaventas, setVentas]);
-//     };
-
-
-
-//     return (
-//         <div className=" flex justify-center items-center">
-//             <form ref={form} onSubmit={submitForm} className=" bg-white px-6 py-3 shadow-2xl">
-//                 <div>
-//                     <h2 class="text-3xl text-center pb-10 text-principal ">Registro de Ventas</h2>
-//                 </div>
-//                 <div className="flex space-x-4 mb-3">
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="fecha">Fecha</label>
-//                         <input
-//                             className="input"
-//                             type="date"
-//                             name="fecha"
-//                             required
-//                         />
-//                     </div>
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="idProducto">Id Producto</label>
-//                         <input
-//                             className="input"
-//                             type="number"
-//                             name="idProducto"
-//                             required
-//                         />
-//                     </div>
-//                 </div>
-//                 <div className="flex space-x-4 mb-3">
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="id">Identificador</label>
-//                         <input
-//                             className="input"
-//                             type="number"
-//                             name="id"
-//                             required
-//                         />
-//                     </div>
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="nombreProducto">Nombre Producto</label>
-//                         <input
-//                             className="input"
-//                             type="text"
-//                             name="nombreProducto"
-//                             required
-//                         />
-//                     </div>
-//                 </div>
-//                 <div className="flex space-x-4 mb-3">
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="idCliente">Id Cliente</label>
-//                         <input
-//                             className="input"
-//                             type="number"
-//                             name="idCliente"
-//                             required
-//                         />
-//                     </div>
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="cantidadProducto">Cantidad Producto</label>
-//                         <input
-//                             className="input"
-//                             type="number"
-//                             name="cantidadProducto"
-//                             required
-//                         />
-//                     </div>
-//                 </div>
-//                 <div className="flex space-x-4 mb-3">
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="nombreCliente">Nombre Cliente</label>
-//                         <input
-//                             className="input"
-//                             type="text"
-//                             name="nombreCliente"
-//                             required
-//                         />
-//                     </div>
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="precioUnitario">Precio unitario</label>
-//                         <input
-//                             className="input"
-//                             type="number"
-//                             name="precioUnitario"
-//                             required
-//                         />
-//                     </div>
-//                 </div>
-//                 <div className="flex space-x-4 mb-3">
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="nombreVendedor">Nombre Vendedor</label>
-//                         <input
-//                             className="input"
-//                             type="text"
-//                             name="nombreVendedor"
-//                             required
-//                         />
-//                     </div>
-//                     <div className="w-1/2">
-//                         <label className="pl-3" htmlFor="totalVenta">Total Venta</label>
-//                         <input
-//                             className="input"
-//                             type="number"
-//                             name="totalVenta"
-//                             required
-//                         />
-//                     </div>
-//                 </div>
-//                 <div className="flex space-x-4 justify-center">
-//                     <div className="w-1/4 ">
-//                         <div>
-//                             <button type="submit" className="boton my-6 w-full">
-//                                 Registrar
-//                             </button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// };
-
-// const ListVentas = ({ listaventas }) => {
-
-//     useEffect(() => {
-//         console.log("ventas:", listaventas);
-//     }, [listaventas]);
-
-//     return (
-//         <>
-//             <div className="bg-white px-6 py-3 shadow-2xl">
-//                 <div>
-//                     <h2 class="text-3xl text-center pb-10 text-principal ">Listado de Ventas</h2>
-//                 </div>
-//                 <table className="tabla">
-
-//                 <thead>
-//                         <tr>
-//                             <th >Id</th>
-//                             <th >Fecha</th>
-//                             <th >Nombre Producto</th>
-//                             <th >Id Cliente</th>
-//                             <th >Nombre Cliente</th>
-//                             <th >Cantidad Producto</th>
-//                             <th >Precio Unitario</th>
-//                             <th >Nomobre Vendedor</th>
-//                             <th >Total Venta</th>
-//                             <th >Acciones</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                         {
-//                             listaventas.map((venta) => {
-//                                 return (
-//                                     <tr key={nanoid()}>
-//                                         <th>{venta.id}</th>
-//                                         <td>{venta.fecha}</td>
-//                                         <td>{venta.nombreProducto}</td>
-//                                         <td>{venta.idCliente}</td>
-//                                         <td>{venta.nombreCliente}</td>
-//                                         <td>{venta.cantidadProducto}</td>
-//                                         <td>{venta.precioUnitario}</td>
-//                                         <td>{venta.nombreVendedor}</td>
-//                                         <td>{venta.totalVenta}</td>
-//                                         <td className="bg-paleta5">
-//                                             <div className="flex w-full justify-around ">
-//                                                 <i className="fas fa-trash" />
-//                                                 <i className="fas fa-edit" />
-//                                             </div>
-//                                         </td>
-//                                     </tr>
-//                                 )
-//                             })
-//                         }
-//                     </tbody>
-//                 </table>
-//             </div>
-//         </>
-//     )
-// }
-// export default Ventas;
-
-
-
-
 const Ventas = () => {
   const [mostrarLista, setMostrarLista] = useState(true);
   const [ventas, setVentas] = useState([]);
   const [cambioBoton, setCambioBoton] = useState('Crear Nuevo Vehículo');
+  const [ActualizarDatos, setActualizarDatos] = useState(true);
 
   useEffect(() => {
-    //obtener/GET lista de ventas desde el backend
-
     const getVentas = async () => {
       // const getVentas = async () => {
 
@@ -321,9 +24,18 @@ const Ventas = () => {
         console.error(error);
       });
     }
+    if (ActualizarDatos) {
+      getVentas();
+      setActualizarDatos(false);
+    }
+
+  }, [ActualizarDatos])
+
+  useEffect(() => {
+    //obtener/GET lista de ventas desde el backend
 
     if (mostrarLista) {
-      getVentas();
+      setActualizarDatos(true);
     }
 
   }, [mostrarLista]);
@@ -345,7 +57,7 @@ const Ventas = () => {
           Área de Administación de Ventas
         </h1>
         {mostrarLista ? (
-          <ListVentas listaventas={ventas} />
+          <ListVentas listaventas={ventas} setActualizarDatos={setActualizarDatos} />
         ) : (
           <RegisVentas
             setMostrarLista={setMostrarLista}
@@ -367,7 +79,7 @@ const Ventas = () => {
   );
 };
 
-const RegisVentas = ({ setMostrarLista, listaventas, ventas }) => {
+const RegisVentas = ({ setMostrarLista, setActualizarDatos  }) => {
   const form = useRef(null);
 
   const submitForm = async (e) => {
@@ -401,6 +113,7 @@ const RegisVentas = ({ setMostrarLista, listaventas, ventas }) => {
       .request(options).then(function (response) {
         console.log(response.data);
         toast.success('venta registrada con éxito');
+        setActualizarDatos(true);
       }).catch(function (error) {
         console.error(error);
         toast.error('Error al registrar una Venta');
@@ -530,7 +243,7 @@ const RegisVentas = ({ setMostrarLista, listaventas, ventas }) => {
   )
 };
 
-const ListVentas = ({ listaventas }) => {
+const ListVentas = ({ listaventas, setEditar, setActualizarDatos }) => {
 
   const form = useRef(null);
 
@@ -555,15 +268,6 @@ const ListVentas = ({ listaventas }) => {
       headers: { 'Content-Type': 'application/json' },
       data: {
         id: editarVenta._id,
-        fecha: editarVenta.fecha,
-        idProducto: editarVenta.idProducto,
-        nombreProducto: editarVenta.nombreProducto,
-        idCliente: editarVenta.idCliente,
-        cantidadProducto: editarVenta.cantidadProducto,
-        nombreCliente: editarVenta.nombreCliente,
-        precioUnitario: editarVenta.precioUnitario,
-        nombreVendedor: editarVenta.nombreVendedor,
-        totalVenta: editarVenta.totalVenta
       }
     };
 
@@ -571,6 +275,7 @@ const ListVentas = ({ listaventas }) => {
       .request(options).then(function (response) {
         console.log(response.data);
         toast.success('venta editada con éxito');
+        setEditar(false)
       }).catch(function (error) {
         console.error(error);
         toast.error('Error al registrar una Venta');
@@ -605,7 +310,7 @@ const ListVentas = ({ listaventas }) => {
               {
                 listaventas.map((ventas) => {
                   return (
-                    <EditarVenta key={nanoid()} ventas={ventas} />
+                    <EditarVenta key={nanoid()} ventas={ventas}  setActualizarDatos={setActualizarDatos} />
                   )
                 })
               }
@@ -617,11 +322,28 @@ const ListVentas = ({ listaventas }) => {
   )
 };
 
-const EditarVenta = ({ ventas }) => {
+const EditarVenta = ({ ventas, editarFila, setActualizarDatos }) => {
 
   const [editar, setEditar] = useState(false);
-  const [eliminar, setEliminar] = useState(true);
+  
 
+  const eliminarVenta = async () => {
+    
+    const options = {
+      method: 'DELETE',
+      url: 'http://localhost:5000/ventas/eliminar',
+      headers: {'Content-Type': 'application/json'},
+      data: { id: ventas._id }
+    };
+
+    await axios
+    .request(options).then(function (response) {
+      console.log(response.data);
+      setActualizarDatos(true);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }
   return (
     <tr>
 
@@ -662,7 +384,7 @@ const EditarVenta = ({ ventas }) => {
 
               <button type='submit'>
                 <i
-                  onClick={() => { setEditar(!editar) }}
+                  onClick={() => { editarFila() }}
                   className="fas fa-check-square"></i>
               </button>
 
@@ -673,7 +395,7 @@ const EditarVenta = ({ ventas }) => {
             )
           }
 
-          <i onClick={() => { setEliminar(!eliminar) }} className="fas fa-trash" />
+          <i onClick={() => { eliminarVenta() }} className="fas fa-trash" />
         </div>
       </td>
     </tr>
