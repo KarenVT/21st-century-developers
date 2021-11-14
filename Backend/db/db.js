5380c3cf719b4ca2b902dc6a209a5bc4029e6d19
Mileny Suarez <sgmileny755@gmail.com>
2021-11-14 09:54:48 -0500

    Modificación de archivos

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 
-dotenv.config({path: './.env'});
+dotenv.config({ path: './.env' });
 
 const conexion = process.env.BD_URL;
 
